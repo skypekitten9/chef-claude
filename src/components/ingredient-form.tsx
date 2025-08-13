d9117c4658f9a237ff59890f45516b209774fd50
skypekitten9 <github_.3w93q@dralias.com>
2025-08-13 11:27:58 +0200

    use action instead of onSubmit

--- src/components/ingredient-form.tsx
+++ src/components/ingredient-form.tsx
@@ -1,4 +1,3 @@
-import { type FormEvent } from "react";
 import "./ingrident-form.css";
 
 type TIngredientForm = {
@@ -7,19 +6,15 @@ type TIngredientForm = {
 
 export function IngredientForm({ onIngredientSubmited }: TIngredientForm) {
   const INPUT_INGREDIENT_NAME = "ingridient";
-  const handleOnSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const form = event.currentTarget;
-    const formData = new FormData(form);
+  const submitAction = (formData: FormData) => {
     const ingredient = formData.get(INPUT_INGREDIENT_NAME);
     if (ingredient) {
       onIngredientSubmited(ingredient.toString());
     }
-    form.reset();
   };
 
   return (
-    <form onSubmit={handleOnSubmit} className="ingridient-root">
+    <form action={submitAction} className="ingridient-root">
       <input
         type="text"
         className="ingridient-input"
